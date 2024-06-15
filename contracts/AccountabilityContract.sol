e0beffe146fc971ab2ae84edf30b1da0893fdb22
Sardius <0xsardius@gmail.com>
2024-06-15 07:15:28 -0400

    base contract complete

--- contracts/AccountabilityContract.sol
+++ contracts/AccountabilityContract.sol
@@ -27,8 +27,45 @@ contract AccountabilityContract {
         require(msg.value > 0, "You need to send some ether");
     }
 
+    // Safety measure to make sure funds don't get stranded in the contract
     function withdrawFunds() public onlyOwner {
         uint256 amount = address(this).balance;
         require(amount > 0, "There are no funds to withdraw");
+        payable(owner).transfer(amount);
+    }
+
+    function allTasksCompleted() private view returns (bool) {
+        for (uint256 i = 0; i < tasks.length; i++) {
+            if (!tasks[i].isCompleted) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function clearTasks() private onlyOwner {
+        delete tasks;
+    }
+
+    function completeTasks(uint256 _taskId) public onlyOwner {
+        require(_taskId < tasks.length, "Task doesn't exist");
+        require(!tasks[_taskId].isCompleted, "Task is already completed");
+
+        tasks[_taskId].isCompleted = true;
+
+        // withdraws funds and clear tasks if all tasks completed
+        if (allTasksCompleted()) {
+            uint256 amount = address(this).balance;
+            payable(owner).transfer(amount);
+            clearTasks();
+        }
+    }
+
+    function getTaskCount() public view returns (uint256) {
+        return tasks.length;
+    }
+
+    function getDepositAmount() public view returns (uint256) {
+        return address(this).balance;
     }
 }
